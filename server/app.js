671f6aa81530d023f651dac0bd6795e138816de3
funnyboy-roks <funnyboyroks@gmail.com>
2022-03-18 20:56:02 -0500

    Add a simple express server

---
+++ server/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(express.static('dist'));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
